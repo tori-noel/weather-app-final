891b6364b223ef84bb841dc0d083482bf54d4937
tori-noel <124933396+tori-noel@users.noreply.github.com>
2023-03-17 15:40:06 -0400

    updated the time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function showTemperature(response) {
   console.log(response.data.temperature.current);
   let temperatureElement = document.querySelector("#temperature");
@@ -10,6 +33,8 @@ function showTemperature(response) {
   humidityElement.innerHTML = response.data.temperature.humidity;
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  let timeElement = document.querySelector("#date");
+  timeElement.innerHTML = formatDate(response.data.time * 1000);
   console.log(response.data);
 }
 let city = "Paris";
